498734732c86c5690aa67f22ca33db3fd393c79f
LucasFcode <lucasfer.flores@gmail.com>
2024-11-10 16:44:32 -0300

    funciones.h

--- JuegoDados/funciones.h
+++ JuegoDados/funciones.h
@@ -2,6 +2,18 @@
 #include <string>
 using namespace std;
 
+// funcion para mostrar el menu
+int menu_greed (int opcion ) ;
+
+ // funcion para que elija opciones
+ void seleccion_Opcion ( int opcion );
+
+ void modo_unjugador (int opcion);
+
+/*void mostrar_menu ( ) ;
+
+void modo_un_jugador();
+*/
 
 void unJugador(string nombreJugador);
 void saludar (string nombreJugador);
