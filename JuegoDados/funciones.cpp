b4a6ea09e90a18521017f8c4e6e5a5eb1f7bef99
LucasFcode <lucasfer.flores@gmail.com>
2024-11-10 18:15:16 -0300

    funciones.cpp

--- JuegoDados/funciones.cpp
+++ JuegoDados/funciones.cpp
@@ -1,80 +1,143 @@
 #include <iostream>
-#include "funciones.h"
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+#include "menu.h"
+// funcion para tirar los dados
 
-///Primero saludamos al jugador
-void saludar(string nombreJugador){
-      cout<<endl;
-      cout <<" Hola "<< nombreJugador <<" que tengas buena suerte "<<endl;
-      cout <<"---------------------------------------"<<endl;
+int tirar_dados ( ) {
+
+    return rand()  %6 +1 ;
+
+
 }
 
-void unJugador (string nombreJugador){
-    saludar(nombreJugador);
 
-    }
+// funcion para modo un jugador
+  void modo_unjugador (int opcion) {
+      string nombre1;
+      int bloqueador1;
+      int bloqueador2;
+       cout<<"DIGITE  SU NOMBRE "<<endl;
+       cin>>nombre1;
+       cout<<"HOLA "<<nombre1<<" AHORA VAMOS A TIRAR TUS BLOQUEADORES! "<<endl;
 
-    ///------------------------------------------------------------------------------
+       bloqueador1=tirar_dados();
+        cout<< " EL BLOQUEADOR 1 ES:  "<<bloqueador1<<endl;
+         bloqueador2=tirar_dados();
+        cout<< " EL BLOQUEADOR 2 ES:  "<<bloqueador2<<endl;
+        int i,respuesta,j;
+          int puntaje_tirada=0;
 
-    ///primero tirar los bloqueadores
+        for  ( i=1; i<=3;i++) {
 
-    int randombloq(){
-    return rand ()%6+1;
-    }
+            cout<<" RONDA : " <<i<<endl;
 
-void tiradaBloqueadores(int vbloqueadores [], int cantidad){
+                 bool sigue=true;
+                 int dados_para_jugar=5;
+                  int  dados_para_bloquear=0;
+                  int puntaje_de_la_ronda=0;
+                    //////////////////// PARA MANEJAR LAS TIRADAS Y LOS DESCUENTOS DE DADOS
+                     while (dados_para_jugar>0 && sigue==true) {
+                      int puntaje_tirada=0;
+                      int dados_para_bloquear=0;
 
-    for ( int i=0; i<cantidad; i++){
-        vbloqueadores[i]=randombloq();
-     }
-}
-void mostrarBloqueadores (int vbloqueadores [], int cantidad){
 
-    for ( int i=0; i<cantidad; i++){
-        cout << " " << vbloqueadores[i]<<endl;
-        cout << " Este es un bloqueador!!! "<<endl;
-        cout << "--------------------------------"<<endl;
+                      cout<<"TIRADA: "<<endl;
+                      int puntaje_total=0;
+                      /// HACEMOS EL FOR PARA CONTROLAR EL PUNTAJE DE LAS TIRADAS Y BLOQUEAR
+                      for (j=0; j<dados_para_jugar;j++) {
+                      int dados_tirada=0;
+                      dados_tirada=tirar_dados();
+
+                     cout<<"DADOS: "<<dados_tirada<<endl;
+
+                      if (dados_tirada==bloqueador1||dados_tirada==bloqueador2) {
+                      dados_para_bloquear++; /// contamos los dados que vamos bloqueando
+                       }
+                       else {
+                        puntaje_tirada=puntaje_tirada+dados_tirada;
+                       }
+
+                }
+
+                  dados_para_jugar=dados_para_jugar-dados_para_bloquear;
+                  puntaje_de_la_ronda=puntaje_de_la_ronda+puntaje_tirada;
+                cout<<"DADOS QUE TE QUEDAN: " <<dados_para_jugar<<endl;
+                cout<<"EL PUNTAJE DE LA TIRADA ES :  "<<puntaje_tirada<<endl;
+                cout<<"EL PUNTAJE DE LA RONDA ES: "<<puntaje_de_la_ronda<<endl;
+
+
+                if (dados_para_jugar>0) {
+                    cout<<" QUERES SERGUIR JUGANDO, digita 1? "<<endl;
+                cin>>respuesta;
+                }
+                if (respuesta==1) {
+                 sigue=true;
+                }
+              else {
+                sigue=false;
+                }
+
+                if (dados_para_jugar<=0 ) {
+                    cout<<"TE QUEDASTE SIN DADOS!"<<endl;
+                    puntaje_de_la_ronda=0;
+                    cout<<"PUNTAJE DE RONDA 0"<<endl;
+                    puntaje_total= puntaje_total+puntaje_de_la_ronda;
+                    cout<<"PUNTAJE TOTAL ACUMULADO= "<<puntaje_total<<endl;
+                }
+
+            }  ///SALGO DEL WHILE
+
+
+
+              }
+
+
+
+
+
+
         }
-} ///hasta aca los bloqueadores
 
-///------------------------------------------------------------------------------
-///ahora la parte de tirar los 5 dados de un jugador
 
-int randomPlayer1(){
-    return rand ()%6+1;
-    }
 
-void tiradaPlayer1(int vPlayer1 [], int cantidadP){
 
-    for ( int i=0; i<cantidadP; i++){
-        vPlayer1[i]=randomPlayer1();
-     }
+
+
+
+
+// FUNCION PARA MOSTRAR EL MENU
+int menu_greed (int opcion ) {
+
+cout<<"* * * * * OPCIONES * * * "<<endl;
+cout<<"* * * OPCION 1 -> J U G A R * * *"<<endl;
+cout<<"* * * OPCION 2 -> J U G A R * * *"<<endl;
+cout<<"* * * OPCION 3 -> J U G A R * * *"<<endl;
+cin>>opcion;
+
+return opcion;
+
 }
-void mostrarPlayer1 (int vPlayer1 [], int cantidadP){
-
-    for ( int i=0; i<cantidadP; i++){
-        cout << " " << vPlayer1[i]<<endl;
-        }
-        cout << " Estos son tus dados "<<endl;
-} ///hasta aca jugada player1
-///-----------------------------------------------------------------------------
 
 
+void seleccion_Opcion ( int opcion ) {
 
-void jugarBloqueadores (){
-int bloqueadores [2];
+      switch (opcion) {
+        case  1:
+            system ("cls");
+            cout<<"BIENVENIDO AL JUEGO"<<endl;
+              modo_unjugador (opcion) ;
+           break ;
 
-     tiradaBloqueadores(bloqueadores,2);
-     mostrarBloqueadores(bloqueadores,2);
-    }
+        case 2:
+            break;
+
+        case 0:
+            cout<<"FIN, GRACIAS POR JUGAR"<<endl;
+      }
 
 
 
-void jugarPlayer1(){
-int Player1 [5];
 
-    tiradaPlayer1 (Player1,5);
-    mostrarPlayer1 (Player1,5);
-    }
-
+ }
