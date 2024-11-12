00894ce2c1e4bd633c9b02fcac89819caf07f5c8
LucasFcode <lucasfer.flores@gmail.com>
2024-11-12 18:37:33 -0300

    Add files via upload

---
+++ JuegoDados/mainnmainnn.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "funciones.h"
+
+using namespace std;
+
+
+
+    void saludar(string nombreJugador){
+      cout<<endl;
+      cout <<" Hola "<< nombreJugador <<" que tengas buena suerte "<<endl;
+
+      cout <<"---------------------------------------"<<endl;
+}
+
+void multiplayer (string nombreJugador){
+    saludar(nombreJugador);}
+
+
+
