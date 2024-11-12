4a0b5ab0d766937ad1b3e2b60b86073ea7fd2304
emmanuel <emmanuel.sansberoo@gmail.com>
2024-11-12 13:28:30 +0000

    En main: -Se agrego menu para test -funciones guardar y mostrar estadistica

--- JuegoDados/funciones.h
+++ JuegoDados/funciones.h
@@ -6,3 +6,5 @@ using namespace std;
 void multiplayer(string nombreJugador);
 void saludar (string nombreJugador);
 void partidaMultiplayer();
+void guardarEstadistica(string nombreJugador, int puntajeMax);
+void mostrarEstadistica();
--- JuegoDados/main.cpp
+++ JuegoDados/main.cpp
@@ -7,12 +7,33 @@ using namespace std;
 
 
 int main()
-{
+{  
     srand(time(0));
+    int opcion;
 
-    partidaMultiplayer();
+    do {
+        cout << "\n--- Menu ---" << endl;
+        cout << "1. Jugar :)" << endl;
+        cout << "2. Estadisticas" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Elija una opción: ";
+        cin >> opcion;
 
-
+        switch (opcion) {
+            case 1:
+                partidaMultiplayer();
+                break;
+            case 2:
+                mostrarEstadistica();
+                break;
+            case 3:
+                cout << "Saliendo..." << endl;
+                break;
+            default:
+                cout << "Elija una opcion valida:" << endl;
+        }
+    } while (opcion != 3);
 
     return 0;
+
 }
