c286f87dc148df5cb8242e1fed0fc9e5c538a9ab
LucasFcode <lucasfer.flores@gmail.com>
2024-11-13 00:48:37 -0300

    funciones.h

--- JuegoDados/funciones.h
+++ JuegoDados/funciones.h
@@ -3,11 +3,29 @@
 using namespace std;
 
 
-void multiplayer(string nombreJugador);
-void saludar (string nombreJugador);
+
+// funcion para mostrar el menu
+int menu_greed (int opcion ) ;
+
+ // funcion para que elija opciones
+
+ void seleccion_Opcion ( int opcion );
+
+ void modo_unjugador ();
+
+
+/*void mostrar_menu ( ) ;
+
+void modo_un_jugador();
+*/
+
+////FUNCIONES CON RLUTIL
+ void Limpiar_menu();
+ void Dibujar_menu(int y);
+ void Dibujar_titulo();
+ void multiplayer();
+void saludar ();
 void partidaMultiplayer();
-void guardarEstadistica(string nombreJugador, int puntajeMax);
-void mostrarEstadistica();
 void iniciarPartida(bool dosJugadores);
 int tiradaPlayer1(int bloqueador1,int bloqueador2);
 int tiradaPlayer2(int bloqueador1_2,int bloqueador2_2);
